2fc482312f88cac6adce3e5dc66b18db86855081
YamaguchiMasumi6 <yoikaze0619@gmail.com>
2023-04-18 11:41:28 +0900

    lesson1_9 問題文の変数名playをstudyに修正

--- curriculum/src/curriculum_now/lesson1_9/ConditionalExpression.java
+++ curriculum/src/curriculum_now/lesson1_9/ConditionalExpression.java
@@ -34,7 +34,7 @@ public class ConditionalExpression {
 //		②変数 study を定義し、文字列 "カリキュラム" を代入する。
 //		③もし変数 isTired が 0 であれば「私はとても元気です。」、そうでなければ「私は疲れています。」と表示する。
 //		※④は System.out.println() のカッコ内に条件演算子（三項演算子）を使って、一行で書いてください。
-//		④もし変数 isTired が 0 であれば、変数 play を利用して「カリキュラムを続けます。」と表示する。
+//		④もし変数 isTired が 0 であれば、変数 study を利用して「カリキュラムを続けます。」と表示する。
 		int isTired = 1; // 0か1かは選んで記述
 		String study = "カリキュラム";
 		if(isTired == 0) {
